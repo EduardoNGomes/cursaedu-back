57523bd5600644e6d4c07a2446356c4240aceb9f
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-26 10:50:22 -0300

    feat: implement zod validdation pipe

---
+++ src/infra/http/pipe/ZodValidation.ts
@@ -0,0 +1,22 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common'
+import { ZodError, ZodSchema } from 'zod'
+import { fromZodError } from 'zod-validation-error'
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+  transform(value: unknown) {
+    try {
+      return this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: fromZodError(error),
+          message: 'Validation failed',
+          statusCode: 400,
+        })
+      }
+      throw new BadRequestException('Validation failed')
+    }
+  }
+}
