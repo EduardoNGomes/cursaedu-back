bb2c4454f03dadfa5d42fe57b13663fd285af1f2
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-26 08:54:21 -0300

    feat: implement method to send images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..','..', 'public'),
+      serveStaticOptions:{
+        index:false,
+      }
+      
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
