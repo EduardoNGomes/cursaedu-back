561e8eeac3acad3bf5b2422225ad41996ea818e4
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-26 10:48:59 -0300

    feat: implemente http.module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,9 +1,16 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common'
+import { DatabaseModule } from '../database/database.module'
+import { FindAllProductUseCase } from '@/domain/products/application/useCases/FindAllProductsUseCase'
+import { FindProductByIdUseCase } from '@/domain/products/application/useCases/FindProductById'
+import { FindProductByCategoryUseCase } from '@/domain/products/application/useCases/FindProductsByCategoryUseCase'
 
 @Module({
-  imports: [
+  imports: [DatabaseModule],
+  providers: [
+    FindAllProductUseCase,
+    FindProductByIdUseCase,
+    FindProductByCategoryUseCase,
   ],
   controllers: [],
-  providers: [],
 })
 export class HttpModule {}
